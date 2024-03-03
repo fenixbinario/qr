c3dace4154518e3c37817c7862873d857aadb3cb
F̸̖͋̆Ë̸̤́̇N̴̢̢̦̩̜̦͉̟͍̤͋̓͝Ỉ̴̮̳̯̣͎̬͍͓̄̅̆̀̈́͐͗͝Ẍ̴̱̲̖͈͆̈́́̈̓͘ ̴̧̠̮̯̞̺̮̝͒̌̎̆̄̎͂̾͛̕ͅB̴̨̟̼̟̺̯̏ͅI̶̘̜̜̲͓͚̪͌͆͂͘͠ͅN̵̝̬̳̽̿̉̃Ǎ̸̧̨͓̤̪̺̬̰̋̈́̈̏̄̓̽͜Ŗ̵̞̣̻̙̖̥̘̼͑͗̀̃Ì̴͕̻̮̟̣͔̮̓̅͜Ớ̵͙̹̲͇͙̾̃̓̈́̂͌̕͜͠ <fbamperius@gmail.com>
2024-03-03 20:12:00 +0100

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+/*
+This is your site JavaScript code - you can add interactivity and carry out processing
+- Initially the JS writes a message to the console, and moves a button you can add from the README
+*/
+
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+console.log("Hello 🌎");
+
+
+//Source: https://davidshimjs.github.io/qrcodejs/
+
+
+var qrcode = new QRCode("qrcode");
+
+function makeCode () {    
+  var elText = document.getElementById("text");
+  
+  if (!elText.value) {
+    alert("Input a text");
+    elText.focus();
+    return;
+  }
+  
+  qrcode.makeCode(elText.value);
+}
+
+makeCode();
+
+$("#text").
+  on("blur", function () {
+    makeCode();
+  }).
+  on("keydown", function (e) {
+    if (e.keyCode == 13) {
+      makeCode();
+    }
+  });
